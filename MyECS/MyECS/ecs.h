a312ba8c6022ae96960651af59e9c9aadb310de9
千年的霜雪 <1371684195@qq.com>
2025-11-19 09:53:01 +0800

    update

--- MyECS/MyECS/ecs.h
+++ MyECS/MyECS/ecs.h
@@ -3,10 +3,31 @@
 #include <vector>
 #include <algorithm>
 #include <cassert>
+#include <unordered_map>
 
 #define assertm(msg, expr) assert(((void)msg, expr))
 
 namespace ecs {
+
+    using ComponentID = uint32_t;
+    using Entity = uint32_t;
+
+    class IndexGetter final
+    {
+    public:
+        template<typename T>
+        ComponentID Get()
+        {
+            static ComponentID id = curIdx_++;
+            return id;
+        }
+    private:
+        /**
+         * C++17新特性，inline修饰的static变量可以在头文件中定义，不需要在cpp文件中初始化
+         */
+        inline static ComponentID curIdx_ = 0;    
+    };
+    
     class World final
     {
     public:
@@ -27,6 +48,7 @@ namespace ecs {
 
             Pool(CreateFunc create, DestroyFunc destroy) : create(create), destroy(destroy) { }
 
+            // 拿出元素
             void* Create()
             {
                 if (!cache.empty())
@@ -41,6 +63,7 @@ namespace ecs {
                 return instances.back();
             }
 
+            // 删除元素
             void Destroy(void* elem)
             {
                 if (auto it = std::find(instances.begin(), instances.end(), elem);
@@ -56,5 +79,10 @@ namespace ecs {
                 }
             }
         };
+
+        
+        
+        using ComponentPool = std::unordered_map<ComponentID, Pool>;
+        ComponentPool pool_;
     };  
 }
